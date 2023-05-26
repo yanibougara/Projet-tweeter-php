c29e198cb1f86003bdbc4143777530fe674d04ad
yanibougara <115211844+yanibougara@users.noreply.github.com>
2023-05-26 19:09:50 +0200

    Delete ProjetCDI directory

--- ProjetCDI/js/app.js
+++
@@ -1,4 +0,0 @@
-function togglePopup(){
-    let popup = document.querySelector("#popup-overlay");
-    popup.classList.toggle("open");
-} 
--- ProjetCDI/php/session.php
+++
@@ -1,9 +0,0 @@
-
-
-
-<?php
-
-session_start();
-var_dump($_SESSION);
-
-?>
